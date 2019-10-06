00c90671639a8d9e23cd3c9171929d118222f8c5
Zachary Newton <zacharyrnewton@gmail.com>
2019-10-06 11:51:50 -0400

    added 404 to admin

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Router } from "@reach/router"
 import Layout from "../components/admin-layout"
 import PrivateRoute from "../components/PrivateRoute"
+import NotFound from "./404"
 import AdminPage from "../components/admin/index"
 import AddPodcast from "../components/admin/add"
 import Login from "./login"
@@ -10,6 +11,7 @@ import Login from "./login"
 const Admin = () => (
   <Layout>
     <Router>
+      <NotFound default/>
       <Login path="/login/"/>
       <PrivateRoute path="/admin/" component={AdminPage} />
       <PrivateRoute path="/admin/add/" component={AddPodcast} />

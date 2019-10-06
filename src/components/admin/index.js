c84413b531956d84e77fdaedc309c1f610d70dc2
Zachary Newton <zacharyrnewton@gmail.com>
2019-10-06 11:15:03 -0400

    podcast list styles

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -9,24 +9,90 @@ const IndexPage = () => (
     <SEO title="Admin" />
     <div className={style.contentWrapper}>
       <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
-      <h1>Podcasts</h1>
+      <ul className={style.adminPodcastList}>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+        <Link to="/admin/podcastID" className={style.adminPodcastListItemLink}>
+          <li className={style.adminPodcastListItem}>
+            <h2 className={style.adminPodcastListItemTitle}>This is the title of the podcast.</h2>
+            <div>
+              <small className={style.adminPodcastListMetaItem}><b>Posted:</b> 04/04/2019</small><small className={style.adminPodcastListMetaItem}><b>Edited:</b> 04/04/2019</small>
+            </div>
+            <p className={style.adminPodcastListItemExcerpt}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem.</p>
+          </li>
+        </Link>
+      </ul>
     </div>
     <div className={style.bottomNavigation}>
       <Link to="/admin/add/" className={style.addPodcast}>Add Podcast</Link>

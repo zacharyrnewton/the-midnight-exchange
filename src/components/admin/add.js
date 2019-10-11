a6329862dcee1b53102834a2a11b13957f477bf6
Zachary Newton <zacharyrnewton@gmail.com>
2019-10-10 22:45:34 -0400

    added staging project

--- src/components/admin/add.js
+++ src/components/admin/add.js
@@ -1,8 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 import SEO from "../seo"
 import style from "../../sass/admin.module.sass"
 import firebase from "../../services/firebase-config"
+
+
+
+function add(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  // Firestore Reference
+  const db = firebase.firestore();
+
+  // Get Form Data
+  const title = document.getElementById('podcastName').value;
+  const description = document.getElementById('podcastDescription').value;
+  const isExplicit = document.getElementById('podcastExplicitYes').value;
+
+  // Post Data
+  db.collection("podcasts").add({
+      title: title,
+      description: description,
+      isExplicit: isExplicit
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+      navigate(/admin/);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });
+};
+
+
 
 const IndexPage = () => (
   <>
@@ -12,7 +42,7 @@ const IndexPage = () => (
       <form className={style.podcastForm}>
         <div className={style.addFile + " " + style.inputWrapper}>
           <label htmlFor="podcastFile">Upload .mp3 file</label>
-          <input type="file" id="podcastFile" name="podcastFile" accept="audio/mpeg, audio/mp4" required/>
+          <input type="file" id="podcastFile" name="podcastFile" accept="audio/mpeg, audio/mp4" />
         </div>
         <div className={style.inputWrapper}>
           <label htmlFor="podcastFile">Title</label>
@@ -41,9 +71,9 @@ const IndexPage = () => (
           <label htmlFor="podcastSchedule">Schedule</label>
           <input type="datetime-local" id="podcastSchedule" name="podcastSchedule"/>
         </div>
-        <button id="btnLogin">Log In</button>
-        <Link to="/admin/" className={style.cancelButton}>Cancel</Link>
+        <button id="btnLogin" onClick={add}>Add Podcast</button>
       </form>
+      <Link to="/admin/" className={style.cancelButton}>Cancel</Link>
     </div>
   </>
 )
